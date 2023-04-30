134e11514ad00b25bb51d5d6f793e35b2b6ac409
Herschel Pawar <pawarherschel@gmail.com>
2023-04-30 20:56:43 +0530

    printing the number of rows now

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ use sqlx_vrchat_sqlite_test::models::connection::establish_connection;
 use sqlx_vrchat_sqlite_test::models::gamelog_join_leave::GamelogJoinLeave;
 use sqlx_vrchat_sqlite_test::models::gamelog_location::GamelogLocation;
 use sqlx_vrchat_sqlite_test::models::usr_friend_log_current::UsrFriendLogCurrent;
+use sqlx_vrchat_sqlite_test::zaphkiel::trust_level::TrustLevel;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let settings = AppConfig::get().build().await?;
     let db = establish_connection(settings.url).await?;
 
-    let _all_gamelog_locations = GamelogLocation::get_all(&db).await?;
+    let all_gamelog_locations = GamelogLocation::get_all(&db).await?;
 
     // println!(
     //     "all_gamelog_locations group name is some:\t{:#?}",
@@ -21,7 +22,37 @@ async fn main() -> Result<(), Box<dyn Error>> {
     //         .collect::<Vec<&GamelogLocation>>()
     // );
 
-    let _all_friends = UsrFriendLogCurrent::get_all(&db, &settings.usr_id).await?;
+    println!(
+        "Number of GamelogLocation rows: {}",
+        all_gamelog_locations.len()
+    );
+    println!(
+        "Number of GamelogLocations with group_name is some: {}",
+        all_gamelog_locations
+            .iter()
+            .filter(|x| x.group_name.is_some())
+            .count()
+    );
+
+    let all_friends = UsrFriendLogCurrent::get_all(&db, &settings.usr_id).await?;
+
+    println!("Number of UsrFriendLogCurrent rows: {}", all_friends.len());
+
+    println!(
+        "Number of UsrFriendLogCurrent with trust level is user: {}",
+        all_friends
+            .iter()
+            .filter(|x| x.trust_level == TrustLevel::User)
+            .count()
+    );
+
+    println!(
+        "Number of UsrFriendLogCurrent with trust level is unknown: {}",
+        all_friends
+            .iter()
+            .filter(|x| x.trust_level == TrustLevel::Unknown)
+            .count()
+    );
 
     // println!(
     //     "friends with trust level user:\t{:#?}",
@@ -39,39 +70,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
     //         .collect::<Vec<&UsrFriendLogCurrent>>()
     // );
 
-    let _all_join_leave = GamelogJoinLeave::get_all(&db).await?;
+    let all_join_leave = GamelogJoinLeave::get_all(&db).await?;
 
-    // println!(
-    //     "join leave where location is none:\t{:#?}",
-    //     all_join_leave
-    //         .iter()
-    //         .filter(|x| x.location.is_none())
-    //         .collect::<Vec<&GamelogJoinLeave>>()
-    // );
-    //
-    // println!(
-    //     "join leave where user is none:\t{:#?}",
-    //     all_join_leave
-    //         .iter()
-    //         .filter(|x| x.user_id.is_none())
-    //         .collect::<Vec<&GamelogJoinLeave>>()
-    // );
-    //
-    // println!(
-    //     "Number of join leaves with location is none: {}",
-    //     all_join_leave
-    //         .iter()
-    //         .filter(|x| x.location.is_none())
-    //         .count()
-    // );
-    //
-    // println!(
-    //     "Number of join leaves with user is none: {}",
-    //     all_join_leave
-    //         .iter()
-    //         .filter(|x| x.user_id.is_none())
-    //         .count()
-    // );
+    println!("Number of GamelogJoinLeave rows: {}", all_join_leave.len());
+
+    println!(
+        "Number of GamelogJoinLeave with location is none: {}",
+        all_join_leave
+            .iter()
+            .filter(|x| x.location.is_none())
+            .count()
+    );
+
+    println!(
+        "Number of GamelogJoinLeave with user is none: {}",
+        all_join_leave
+            .iter()
+            .filter(|x| x.user_id.is_none())
+            .count()
+    );
 
     Ok(())
 }
